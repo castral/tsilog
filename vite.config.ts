7c729476819e18b9d1d0005c2c5bd073b2b59e1c
castral <castral01@gmail.com>
2026-03-04 19:52:28 -0600

    Config adjustments
    Took 32 minutes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-
+    viteTsConfigPaths({
+      projects: [
+        './tsconfig.json',
+        './tsconfig.spec.json',
+      ],
+    }),
   ],
+
   build: {
     outDir: 'dist',
     target: 'es2022',
     sourcemap: true,
     lib: {
-      entry: 'lib/index.ts',
+      entry: './lib/index.ts',
       formats: ['es'],
       fileName: () => 'index.mjs',
     },
@@ -20,13 +27,43 @@ export default defineConfig({
         format: 'esm',
         sourcemap: 'inline',
         strict: true,
-      }
-    }
+      },
+    },
   },
+
   test: {
     typecheck: {
       enabled: true,
       tsconfig: './tsconfig.spec.json',
     },
-  }
-});
+
+    coverage: {
+      include: [
+        './lib/**/*.ts',
+      ],
+      enabled: true,
+      provider: 'v8',
+      reporter: ['html', 'json', 'text'],
+      reportsDirectory: 'coverage',
+      thresholds: {
+        [100]: true,
+      },
+    },
+
+    include: [
+      './spec/**/*.spec.ts',
+    ],
+    setupFiles: ['./spec/spec-setup.ts'],
+    environment: 'node',
+    globals: true,
+    passWithNoTests: false,
+    disableConsoleIntercept: true,
+    printConsoleTrace: true,
+    reporters: ['verbose'],
+    sequence: {
+      concurrent: true,
+    },
+    slowTestThreshold: 1000,
+    watch: false,
+  },
+}) satisfies UserConfig as UserConfig;
